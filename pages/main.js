65503ca528b9861f253c99b4746064c725349a17
Dhruv Solanki <dhruvsolanki@Dhruvs-MacBook-Air-2.local>
2025-09-05 20:42:24 +0530

    initial landing page

---
+++ pages/main.js
@@ -0,0 +1,10 @@
+export default function MainPage() {
+    return (
+      <div className="flex items-center justify-center h-screen bg-gray-50">
+        <h1 className="text-4xl font-bold text-indigo-700">
+          Welcome to the Competition Portal 🚀
+        </h1>
+      </div>
+    );
+  }
+  
